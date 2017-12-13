7b7cb089e24ea9d5a97d480c639348372c37fd2d
brolam <breno@brolam.com.br>
2017-12-13 19:43:09 -0300

    chore: Initial structure; Functional prototype; Added pod 'GoogleMobileVision/FaceDetector'

---
+++ iOS/AppTests/AppTestCase.swift
@@ -0,0 +1,33 @@
+//
+//  BMFaceDetectorTests.swift
+//  CloudVisionTests
+//
+//  Created by Breno Marques on 13/12/2017.
+//  Copyright © 2017 Breno Marques. All rights reserved.
+//
+
+import XCTest
+
+class AppTestCase: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
