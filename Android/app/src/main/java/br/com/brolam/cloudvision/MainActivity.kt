ad739b1660b687cf85e77f6dccbdec134c89f01c
brolam <breno@brolam.com.br>
2018-01-05 11:20:19 -0300

    docs: MainActivity - take a phote and picture library select prototype.

--- Android/app/src/main/java/br/com/brolam/cloudvision/MainActivity.kt
+++ Android/app/src/main/java/br/com/brolam/cloudvision/MainActivity.kt
@@ -1,15 +1,28 @@
 package br.com.brolam.cloudvision
 
+import android.app.Activity
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 
 import kotlinx.android.synthetic.main.activity_main.*
+import android.provider.MediaStore
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Environment
+import android.widget.ImageView
+import android.widget.Toast
+import java.io.IOException
+import java.io.File
+import android.support.v4.content.FileProvider
+
+const val REQUEST_IMAGE_CAPTURE = 5001
+const val REQUEST_IMAGE_SELECT = 5002;
 
 class MainActivity : AppCompatActivity() , View.OnClickListener  {
+    private val tempPicture = "tempPicture"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,23 +37,60 @@ class MainActivity : AppCompatActivity() , View.OnClickListener  {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        /*
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_gallery -> {
+                val galleryIntent = Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+                startActivityForResult(galleryIntent, REQUEST_IMAGE_SELECT)
+                return true
+            }
             else -> super.onOptionsItemSelected(item)
         }
-        */
-        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        val imageView = this.findViewById<ImageView>(R.id.imageView)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val imageBitmap = BitmapFactory.decodeFile(getTempPictureUri())
+            imageView.setImageBitmap(imageBitmap)
+        } else if (requestCode == REQUEST_IMAGE_SELECT && resultCode == RESULT_OK) {
+            if (data != null) {
+                val contentURI = data.data
+                try {
+                    val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
+                    imageView.setImageBitmap(bitmap)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                    Toast.makeText(this@MainActivity, "Failed!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
     override fun onClick(view: View?) {
         if ( view == null ) return
         if ( view.equals(fab)){
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            var pictureFileOutput = createTempPictureUri()
+            val pictureUriOutput = FileProvider.getUriForFile(this,
+                    "br.com.brolam.cloudvision.fileprovider",
+                    pictureFileOutput)
+
+            if (takePictureIntent.resolveActivity(packageManager) != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, pictureUriOutput);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
         }
+    }
+
+    @Throws(IOException::class)
+    private fun createTempPictureUri(): File {
+        return File(getTempPictureUri())
+    }
+
+    private fun getTempPictureUri(): String {
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return storageDir.absolutePath + "/" + tempPicture + ".jpg"
     }
 }
