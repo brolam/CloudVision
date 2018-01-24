b8c3a0c8925a7ce71a2f35ce5341ea607e7ede7d
brolam <breno@brolam.com.br>
2018-01-24 15:34:21 -0300

    refactor: prototype used CrowdPeopleEntity.

--- Android/app/src/main/java/br/com/brolam/cloudvision/FacesActivity.kt
+++ Android/app/src/main/java/br/com/brolam/cloudvision/FacesActivity.kt
@@ -5,22 +5,16 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.util.SparseArray
-import br.com.brolam.cloudvision.helpers.FacesDetector
-import br.com.brolam.cloudvision.helpers.ImageUtil
 import br.com.brolam.cloudvision.viewModels.CrowdViewModel
 import br.com.brolam.cloudvision.views.FaceItemView
-import com.google.android.gms.vision.face.Face
 import kotlinx.android.synthetic.main.activity_faces.*
 
 
 class FacesActivity : AppCompatActivity() {
     private lateinit var crowdViewModel: CrowdViewModel
-    private lateinit var imageUtil: ImageUtil
 
     companion object {
         private const val PARAM_CROWD_ID = "crowd_id"
@@ -36,16 +30,12 @@ class FacesActivity : AppCompatActivity() {
         setContentView(R.layout.activity_faces)
         setSupportActionBar(toolbar)
         this.crowdViewModel = ViewModelProviders.of(this).get(CrowdViewModel::class.java)
-        this.imageUtil = ImageUtil(this)
         val crowdId = this.intent.getLongExtra(PARAM_CROWD_ID, 0)
-        this.crowdViewModel.getCrowdById(crowdId).observe(this, Observer { crowd ->
-            this.imageUtil.getImage(crowd!!.trackedImageName)?.let { trackedImage ->
-                val facesDetector = FacesDetector(this)
-                facesDetector.trackFaces(trackedImage)
-                val trackingFaces = facesDetector.trackingFaces
-                this.fillFlexboxLayoutFaces(trackedImage, trackingFaces)
+        this.crowdViewModel.getCrowdPeopleById(crowdId).observe(this, Observer { crowdPeople ->
+            this.crowdViewModel.getTrackedImage(crowdPeople!!.crowd.trackedImageName)?.let { trackedImage ->
+                this.fillFlexboxLayoutFaces(crowdViewModel.getImagesPeoplefaces(trackedImage,  crowdPeople!!.people ))
                 imageViewTrackedImage.setImageBitmap(trackedImage)
-                textViewTitle.text = crowd.title
+                textViewTitle.text = crowdPeople!!.crowd.title
             }
         })
 
@@ -56,13 +46,7 @@ class FacesActivity : AppCompatActivity() {
 
     }
 
-    private fun fillFlexboxLayoutFaces(trackedImage: Bitmap, trackingFaces: SparseArray<Face>) {
-        val facesBitmap = mutableListOf<Bitmap>()
-        for (index in 0 until trackingFaces.size()) {
-            val face = trackingFaces.valueAt(index)
-            val faceBitmap = this.crop(trackedImage, face, 15.toFloat(), 15.toFloat())
-            facesBitmap.add(faceBitmap)
-        }
+    private fun fillFlexboxLayoutFaces(facesBitmap: List<Bitmap>) {
         flexboxLayoutFaces.removeAllViews()
         facesBitmap.map { face ->
             val faceItemView = layoutInflater.inflate(R.layout.view_face_item, flexboxLayoutFaces, false) as FaceItemView
@@ -72,15 +56,5 @@ class FacesActivity : AppCompatActivity() {
         this.textViewFacesAmount.text = flexboxLayoutFaces.childCount.toString()
     }
 
-    private fun crop(trackedImage: Bitmap, face: Face, enlargeWidthInPercent: Float, enlargeHeightInPercent: Float): Bitmap {
-        return this.imageUtil.crop(
-                trackedImage,
-                face.position.x,
-                face.position.y,
-                face.width,
-                face.height,
-                enlargeWidthInPercent,
-                enlargeHeightInPercent
-        )
-    }
+
 }
--- Android/app/src/main/java/br/com/brolam/cloudvision/viewModels/CrowdViewModel.kt
+++ Android/app/src/main/java/br/com/brolam/cloudvision/viewModels/CrowdViewModel.kt
@@ -5,13 +5,13 @@ import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
 import android.graphics.Bitmap
 import android.os.AsyncTask
-import android.os.Environment
 import android.text.format.DateUtils
+import br.com.brolam.cloudvision.helpers.FacesDetector
 import br.com.brolam.cloudvision.helpers.ImageUtil
 import br.com.brolam.cloudvision.models.AppDatabase
 import br.com.brolam.cloudvision.models.CrowdEntity
-import java.io.File
-import java.io.FileOutputStream
+import br.com.brolam.cloudvision.models.CrowdPeopleEntity
+import br.com.brolam.cloudvision.models.CrowdPersonEntity
 import java.util.*
 
 /**
@@ -25,21 +25,63 @@ class CrowdViewModel(application: Application) : AndroidViewModel(application) {
 
     fun insertCrowd(trackedImageBitmap: Bitmap, onCompleted: (Long) -> Unit) {
         AsyncTask.execute {
+            val facesDetector = FacesDetector(this.getApplication())
+            facesDetector.trackFaces(trackedImageBitmap)
             val created = Date().time
             val title = DateUtils.formatDateTime(
                     this.getApplication(),
                     created,
                     DateUtils.FORMAT_SHOW_DATE + DateUtils.FORMAT_ABBREV_MONTH + DateUtils.FORMAT_SHOW_TIME )
+
+            val trackingFaces = facesDetector.trackingFaces
+
             val trackedImageName = "/crowd_$created.jpg"
             this.imageUtil.save(trackedImageName, trackedImageBitmap)
-            val crowdEntity = CrowdEntity(title = title, trackedImageName = trackedImageName, created = created)
-            val crowdId = this.crowdDao.insert(crowdEntity)
-            if (onCompleted != null) onCompleted(crowdId)
+
+            this.appDatabase.runInTransaction {
+                val crowdEntity = CrowdEntity(title = title, trackedImageName = trackedImageName, created = created)
+                val crowdId = this.crowdDao.insert(crowdEntity)
+                val crowdPeople = mutableListOf<CrowdPersonEntity>()
+                for (index in 0 until trackingFaces.size()) {
+                    val face = trackingFaces.valueAt(index)
+                    val crowdPerson = CrowdPersonEntity(
+                            crowdId = crowdId,
+                            insertedOrder = index,
+                            faceWidth = face.width,
+                            faceHeight = face.height,
+                            facePositionX = face.position.x,
+                            facePositionY = face.position.y)
+                    crowdPeople.add(crowdPerson)
+                }
+                this.crowdDao.insert(crowdPeople)
+                onCompleted(crowdId)
+            }
         }
     }
 
-    fun getCrowdById(id: Long): LiveData<CrowdEntity> {
-        return this.crowdDao.getById(id)
+    fun getCrowdPeopleById(id: Long): LiveData<CrowdPeopleEntity> {
+        return this.crowdDao.getCrowdPeopleById(id)
+    }
+
+    fun getImagesPeoplefaces(trackedImage: Bitmap, people: List<CrowdPersonEntity>) : List<Bitmap> {
+        val facesBitmap = mutableListOf<Bitmap>()
+        people.forEach { crowdPersonEntity ->
+            val faceBitmap = this.imageUtil.crop(
+                    trackedImage,
+                    crowdPersonEntity.facePositionX,
+                    crowdPersonEntity.facePositionY,
+                    crowdPersonEntity.faceWidth,
+                    crowdPersonEntity.faceHeight,
+                    enlargeWidthInPercent = 15.toFloat(),
+                    enlargeHeightInPercent =  15.toFloat())
+            facesBitmap.add(faceBitmap)
+
+        }
+        return facesBitmap
+    }
+
+    fun getTrackedImage(trackedImageName:String): Bitmap? {
+       return this.imageUtil.getImage(trackedImageName)
     }
 
 }
